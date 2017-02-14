56d5c617126e30d7ab5f619c9dfe237f4c25221d
xechoz <zheng1733@gmail.com>
2017-02-14 18:06:47 +0800

    add modle

---
+++ app/src/main/java/xyz/xechoz/demo/App.java
@@ -0,0 +1,11 @@
+package xyz.xechoz.demo;
+
+/**
+ * Created by xechoz.zheng on 2/14/17.
+ * Email: zheng1733@gmail.com
+ * 功能:
+ * 文档:
+ */
+
+public class App {
+}
---
+++ app/src/main/java/xyz/xechoz/demo/database/User.java
@@ -0,0 +1,11 @@
+package xyz.xechoz.demo.database;
+
+/**
+ * Created by xechoz.zheng on 2/14/17.
+ * Email: zheng1733@gmail.com
+ * 功能:
+ * 文档:
+ */
+
+public class User {
+}
