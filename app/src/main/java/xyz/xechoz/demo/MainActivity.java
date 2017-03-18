a4c875d6a4352c0c672bd1f203bf35e65d6eed08
xechoz <zheng1733@gmail.com>
2017-03-18 12:47:47 +0800

    user one

--- app/src/main/java/xyz/xechoz/demo/MainActivity.java
+++ app/src/main/java/xyz/xechoz/demo/MainActivity.java
@@ -1,9 +1,33 @@
 package xyz.xechoz.demo;
 
+import com.google.protobuf.GeneratedMessage;
+import com.orhanobut.logger.Logger;
+
+import butterknife.OnClick;
+import xyz.xechoz.demo.model.entity.Message;
+
 public class MainActivity extends BaseActivity {
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_main;
+    }
+
+    @OnClick(R.id.button)
+    void onClick() {
+        Message.Hello hello = Message.Hello.newBuilder()
+                .setContent("cot")
+                .setId(System.nanoTime())
+                .build();
+
+        Logger.d("hello" + hello);
+
+        hello.toByteArray();
+    }
+
+
+
+    private byte[] toBytyArray(GeneratedMessage message) {
+        return message.toByteArray();
     }
 }
